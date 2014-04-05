5187f1d007baa6589085abf0c3f3e987bd751ebd
Elijah Butterfield <elijah.code@gmail.com>
2014-04-05 13:04:55 -0700

    Fixed Menu Swipe

--- menu.lua
+++ menu.lua
@@ -6,7 +6,7 @@ local scene = storyboard.newScene()
 function scene:createScene( event )
 local group = self.view
 local mainGroup = display.newGroup()
-local sideBarGroup = display.newGroup()
+sideBarGroup = display.newGroup()
 
 
 local mainMenuBackground = display.newRect( _W*0.5, _H*0.5, _W, _H )
@@ -18,29 +18,103 @@ topBar.y = topBar.height/2
 topBar:setFillColor( 244/255,45/255,53/255 )
 mainGroup:insert(topBar)
 
-local sideBarBackground = display.newRect( _W*0.5, _H*0.5, 576, _H )
+sideBarBackground = display.newRect( _W*0.5, _H*0.5, 576, _H )
 sideBarBackground:setFillColor( 229/255,229/255,129/255 )
 sideBarBackground.x = _W*0.5 - sideBarBackground.width - 72
 sideBarGroup:insert(sideBarBackground)
 
-local sideBarTouch = display.newRect( _W*0.5, _H*0.5, 50, _H )
+sideBarTouch = display.newRect( _W*0.5, _H*0.5+48, 300, _H-96 )
 sideBarTouch:setFillColor( 129/255,229/255,129/255 )
-sideBarTouch.x = sideBarBackground.x + 628/2
+sideBarTouch.x = sideBarBackground.x + 608/2
+sideBarTouch.alpha = 0.1
 sideBarGroup:insert(sideBarTouch)
-local sideSwipe = function(event)
-print"side bar touched"
-end
-sideBarTouch:addEventListener("touch", sideSwipe)
+
+
 
 end
 
 --------------------
 
 function scene:enterScene( event )
+local sideBarState = "closed"
+local beginX 
+local beginY  
+local endX  
+local endY 
+ 
+local xDistance  
+local yDistance  
+ 
+function checkSwipeDirection()
+ 
+        xDistance =  math.abs(endX - beginX) -- math.abs will return the absolute, or non-negative value, of a given value. 
+        yDistance =  math.abs(endY - beginY)
+        
+        if xDistance > yDistance then
+                if beginX > endX then
+                    if sideBarState == "open" then
+					sideBarState = "closed"
+					print(sideBarState)
+					transition.to(sideBarGroup, { time=100,x = 0 , transition=easing.inOutQuad })
+                    print("swipe left")
+				end
+                else 
+					if sideBarGroup.x > 10 and sideBarState == "closed" then
+					sideBarState = "open"
+					print(sideBarState)
+					transition.to(sideBarGroup, { time=100,x = 576 , transition=easing.inOutQuad })
+                    print("swipe right")
+					end
+                end
+        else 
+                if beginY > endY then
+                        print("swipe up")
+                else 
+                        print("swipe down")
+                end
+        end
+        
+end
+ 
+ 
+function swipe(event)
+        if event.phase == "began" then
+                beginX = event.x
+                beginY = event.y
+        end
+        
+        if event.phase == "ended"  then
+                endX = event.x
+                endY = event.y
+                checkSwipeDirection();
+        end
+end
+ 
+Runtime:addEventListener("touch", swipe)
 
---
--- Manipulate objects here (e.g. scene logic)
---
+
+local function sideSwipe( event )
+	local t = event.target
+
+	local phase = event.phase
+	if "began" == phase then
+		t.isFocus = true
+
+		-- Store initial position
+
+		sideBarGroup.x0 = event.x - sideBarGroup.x
+	elseif t.isFocus then
+		if "moved" == phase then
+			sideBarGroup.x = event.x - sideBarGroup.x0
+		elseif "ended" == phase or "cancelled" == phase then
+			display.getCurrentStage():setFocus( nil )
+			t.isFocus = false
+
+		end
+	end
+end
+sideBarTouch:addEventListener("touch", sideSwipe)
+
 
 
 
